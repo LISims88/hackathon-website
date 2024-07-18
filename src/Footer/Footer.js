4d9051ac7403d81f649b34e399aa004c8e9a73b2
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-07-18 13:23:32 -0500

    Begin Header and Footer component rendering.

---
+++ src/Footer/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css"
+const Footer =()=>{
+    return(
+        <>
+            <footer>
+                This project was bootstrapped with Create React App. Published 2024.
+            </footer>
+        </>
+    )
+}
+export default Footer
