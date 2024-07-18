ea6dac5acab80787d76d58c8e86ccf3af768e88a
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-07-18 15:56:13 -0500

    further alter header and footer for visibility sake.

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,13 +1,13 @@
 import "./Footer.css"
 const Footer =()=>{
     return(
-        <>
+        <div className="footerContainer">
             <footer>
                 <p>
                 This project was bootstrapped with Create React App. Published 2024.
                 </p>
             </footer>
-        </>
+        </div>
     )
 }
 export default Footer
